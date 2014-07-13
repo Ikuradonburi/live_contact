8868e9f4530e96fdb76b24096be4947a16b5e7b9
Ikuradonburi <wsrngs514@hotmail.co.jp>
2014-07-13 12:44:17 +0900

    #4　多次元配列の設定　$Config

--- lb_config.php
+++ lb_config.php
@@ -1,3 +1,23 @@
 <?php
 
+$Config = array();
+
+$Config["contactPart"] = array(
+	"",
+	"ボーカル",
+	"ギター",
+	"ベース",
+	"キーボード",
+	"ドラム"
+);
+
+$Config["contactPlace"] = array(
+	"",
+	"梅田Zeela",
+	"心斎橋Clapper",
+	"心斎橋Soma"
+);
+
+$Config["contactTime"] = array("","午前","午後","夜");
+
 ?>
