ee43ca63ac4ec43261073041306f81512cae69e1
Ikuradonburi <wsrngs514@hotmail.co.jp>
2014-07-13 12:33:31 +0900

    #4 ログインとメンバートップの表示設定

--- index.php
+++ index.php
@@ -4,4 +4,29 @@ require_once('lb_init.php');
 
 login();
 
+if(empty($_REQUEST["m"])) {
+	
+	$Smarty -> display('mem_top.html');
+	exit;
+
+} else {
+
+	switch($_REQUEST["m"]) {
+		
+		case "contact_form":
+			contact_form("");
+			break;
+
+		case "contact_conf":
+			contact_conf();
+			break;
+
+		case "contact_end":
+			contact_end();
+			break;
+		
+	}
+
+}
+
 ?>
